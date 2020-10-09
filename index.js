457802d7a3d2ef5065e11ac2c009ff67f90c931e
Nick Chubb <Nick.r.chubb@gmail.com>
2020-10-08 21:25:59 -0700

    Init commit

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Routes Definitions
+ */
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+})
+
+/**
+ * Server Activation
+ */
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
